63c257c39c0a1e1421b1af0e6d0d2b7ee20f1d4e
Aleksandr Druzhilov <adruzhilov@unison.com>
2015-08-14 14:45:59 +0300

    Make customizable init script

--- init.sh
+++ init.sh
@@ -2,14 +2,27 @@
 
 CURRENT_DIR=$(pwd)
 
-## Emacs config
-EMACS_DIR=${CURRENT_DIR}/emacs
-mkdir -p ~/.emacs.d
-ln -sf ${EMACS_DIR}/.emacs.d/init.el ~/.emacs.d/init.el
-echo "Emacs config initialized"
-
 ## Vim config
-VIM_DIR=${CURRENT_DIR}/vim
-ln -sf ${VIM_DIR}/.vimrc ~/.vimrc
-echo "Vim config initialized"
+function vim_config {
+    VIM_DIR=${CURRENT_DIR}/vim
+    ln -sf ${VIM_DIR}/.vimrc ~/.vimrc
+    echo "Vim config initialized"
+}
 
+
+## Emacs config
+function emacs_config {
+    EMACS_DIR=${CURRENT_DIR}/emacs
+    mkdir -p ~/.emacs.d
+    ln -sf ${EMACS_DIR}/.emacs.d/init.el ~/.emacs.d/init.el
+    echo "Emacs config initialized"
+}
+
+case "$1" in
+    vim) vim_config
+        ;;
+    emacs) emacs_config
+        ;;
+    *) echo "Please specify config name (vim|emacs|tmux)"
+        ;;
+esac
